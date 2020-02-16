0a18f2500761b60f575bc831de9632c7cc7fa577
Oleh Lokshyn <olokshyn@gmail.com>
2020-02-16 18:32:46 +0200

    Added Game with partial tests

---
+++ game.py
@@ -0,0 +1,104 @@
+import enum
+from typing import Optional
+from collections import OrderedDict
+
+import numpy as np
+
+from grid import Grid
+from chip import Chip
+
+
+class WinState(enum.Enum):
+
+    DRAW = 0
+    GREEN = Chip.GREEN.value
+    RED = Chip.RED.value
+
+
+def check_win_in_vector(vec: np.array, chips_in_a_row):
+    if len(vec) < chips_in_a_row:
+        raise ValueError('Vector is too small')
+    for shift in range(0, len(vec) - chips_in_a_row + 1):
+        check_vec = vec[shift:shift + chips_in_a_row]
+        for win_state in [WinState.GREEN, WinState.RED]:
+            if np.all(check_vec == win_state.value):
+                return win_state
+    return WinState.DRAW
+
+
+def check_win_in_diagonal(matrix: np.array, chips_in_a_row):
+    offset_range = range(
+        -(matrix.shape[0] - chips_in_a_row),
+        (matrix.shape[1] - chips_in_a_row) + 1
+    )
+    for offset in offset_range:
+        diag = matrix.diagonal(offset=offset)
+        win_state = check_win_in_vector(diag, chips_in_a_row)
+        if win_state != WinState.DRAW:
+            return win_state
+    return WinState.DRAW
+
+
+def check_win_in_row(grid: Grid, chips_in_a_row):
+    for grid_row in grid:
+        win_state = check_win_in_vector(grid_row, chips_in_a_row)
+        if win_state != WinState.DRAW:
+            return win_state
+    return WinState.DRAW
+
+
+def check_win_in_column(grid: Grid, chips_in_a_row):
+    for grid_col in grid.T:
+        win_state = check_win_in_vector(grid_col, chips_in_a_row)
+        if win_state != WinState.DRAW:
+            return win_state
+    return WinState.DRAW
+
+
+def check_win_in_main_diag(grid: Grid, chips_in_a_row):
+    return check_win_in_diagonal(grid, chips_in_a_row)
+
+
+def check_win_in_sub_diag(grid: Grid, chips_in_a_row):
+    return check_win_in_diagonal(grid.grid[::-1], chips_in_a_row)
+
+
+class Game:
+
+    def __init__(
+            self,
+            rows_num: int = 6,
+            cols_num: int = 7,
+            chips_in_a_row: int = 4,
+            grid: Optional[Grid] = None
+    ):
+        if grid is not None:
+            self.grid = grid
+        else:
+            self.grid = Grid(rows_num, cols_num)
+        self.chips_in_a_row = chips_in_a_row
+
+    def get_win_state(self):
+        checks = OrderedDict([
+            ('row', check_win_in_row),
+            ('col', check_win_in_column),
+            ('main diag', check_win_in_main_diag),
+            ('sub diag', check_win_in_sub_diag)
+        ])
+        for check_name, check in checks.items():
+            win_state = check(self.grid, self.chips_in_a_row)
+            if win_state != WinState.DRAW:
+                return win_state, check_name
+
+        if self.grid.is_full():
+            return WinState.DRAW
+
+        raise NoWinStateError()
+
+
+class GameError(Exception):
+    pass
+
+
+class NoWinStateError(GameError):
+    pass
---
+++ test/test_game.py
@@ -0,0 +1,383 @@
+from unittest import TestCase
+
+import numpy as np
+
+import grid
+import game
+from game import WinState
+from chip import Chip
+
+
+class TestCheckWinInVector(TestCase):
+
+    def test_too_small_vector(self):
+        self.assertRaises(
+            ValueError,
+            game.check_win_in_vector,
+            vec=np.zeros(3),
+            chips_in_a_row=4
+        )
+        self.assertRaises(
+            ValueError,
+            game.check_win_in_vector,
+            vec=np.zeros(2),
+            chips_in_a_row=4
+        )
+        self.assertRaises(
+            ValueError,
+            game.check_win_in_vector,
+            vec=np.zeros(1),
+            chips_in_a_row=4
+        )
+        self.assertRaises(
+            ValueError,
+            game.check_win_in_vector,
+            vec=np.zeros(0),
+            chips_in_a_row=4
+        )
+        self.assertRaises(
+            ValueError,
+            game.check_win_in_vector,
+            vec=np.zeros(6),
+            chips_in_a_row=7
+        )
+
+    def test_empty_vector(self):
+        self.assertEqual(
+            WinState.DRAW,
+            game.check_win_in_vector(np.zeros(4), 4)
+        )
+        self.assertEqual(
+            WinState.DRAW,
+            game.check_win_in_vector(np.zeros(6), 4)
+        )
+        self.assertEqual(
+            WinState.DRAW,
+            game.check_win_in_vector(np.zeros(6), 6)
+        )
+
+    def test_no_win(self):
+        vec = np.array([1, 0, 1, 2, 1, 0, 2, 2, 2, 1, 1, 0, 1, 1, 2])
+        self.assertEqual(WinState.DRAW, game.check_win_in_vector(vec, 4))
+
+        vec = np.array([1, 1, 1, 2, 2, 2, 1, 0, 1, 0, 1])
+        self.assertEqual(WinState.DRAW, game.check_win_in_vector(vec, 4))
+
+        vec = np.array([2, 2, 2, 2, 2, 1, 1, 1, 1, 1, 2, 2, 0, 2, 2, 2])
+        self.assertEqual(WinState.DRAW, game.check_win_in_vector(vec, 6))
+
+        vec = np.array([1, 1, 1, 2, 2, 2, 1, 2, 1, 2, 1, 2, 1, 1, 0, 1])
+        self.assertEqual(WinState.DRAW, game.check_win_in_vector(vec, 4))
+
+        vec = np.array([0, 2, 2, 2, 2, 1, 1, 1, 1, 0])
+        self.assertEqual(WinState.DRAW, game.check_win_in_vector(vec, 5))
+
+    def test_win_only_green(self):
+        vec = np.array([1, 1, 1, 1, 1, 1, 1, 1, 1, 1])
+        self.assertEqual(WinState.GREEN, game.check_win_in_vector(vec, 4))
+
+        vec = np.array([1, 1, 1, 1, 1, 1, 1, 1, 1, 1])
+        self.assertEqual(WinState.GREEN, game.check_win_in_vector(vec, 7))
+
+        vec = np.array([1, 1, 1, 1, 1, 1, 1, 1, 1, 1])
+        self.assertEqual(WinState.GREEN, game.check_win_in_vector(vec, 10))
+
+    def test_win_only_red(self):
+        vec = np.array([2, 2, 2, 2, 2])
+        self.assertEqual(WinState.RED, game.check_win_in_vector(vec, 5))
+
+        vec = np.array([0, 2, 2, 2, 2, 2, 0, 2, 2, 2, 2, 2])
+        self.assertEqual(WinState.RED, game.check_win_in_vector(vec, 5))
+
+        vec = np.array([0, 0, 0, 2, 2, 2, 0, 2, 2, 2, 2, 2, 0])
+        self.assertEqual(WinState.RED, game.check_win_in_vector(vec, 5))
+
+        vec = np.array([2, 2, 2])
+        self.assertEqual(WinState.RED, game.check_win_in_vector(vec, 3))
+
+    def test_win_at_the_beginning(self):
+        vec = np.array([1, 1, 1, 1, 0, 2, 2, 1, 1, 1, 0, 1, 1])
+        self.assertEqual(WinState.GREEN, game.check_win_in_vector(vec, 4))
+
+        vec = np.array([2, 2, 2, 2, 1, 1, 1, 1, 0, 2, 1, 1])
+        self.assertEqual(WinState.RED, game.check_win_in_vector(vec, 4))
+
+        vec = np.array([1, 1, 1, 1, 1, 1, 1, 8, 9, 2, 2, 1, 1, 2, 2, 2, 2])
+        self.assertEqual(WinState.GREEN, game.check_win_in_vector(vec, 7))
+
+    def test_win_at_the_end(self):
+        vec = np.array([1, 1, 1, 1, 2, 2, 1, 2, 2, 2, 2, 2])
+        self.assertEqual(WinState.RED, game.check_win_in_vector(vec, 5))
+
+        vec = np.array([1, 2, 2, 2, 1, 2, 1, 2, 2, 2, 2])
+        self.assertEqual(WinState.RED, game.check_win_in_vector(vec, 4))
+
+        vec = np.array([0, 1, 1, 0, 2, 2, 1, 1, 1, 1, 1, 1, 1])
+        self.assertEqual(WinState.GREEN, game.check_win_in_vector(vec, 7))
+
+    def test_win_in_the_middle(self):
+        vec = np.array([2, 1, 0, 1, 0, 2, 2, 2, 2, 2, 1, 1, 1, 1, 0])
+        self.assertEqual(WinState.RED, game.check_win_in_vector(vec, 5))
+
+        vec = np.array([1, 1, 1, 0, 2, 2, 2, 2, 0, 1, 1, 1])
+        self.assertEqual(WinState.RED, game.check_win_in_vector(vec, 4))
+
+        vec = np.array([2, 2, 2, 2, 2, 0, 0, 0, 1, 1, 1, 1, 1, 1, 2, 2, 0])
+        self.assertEqual(WinState.GREEN, game.check_win_in_vector(vec, 6))
+
+
+class TestCheckWinInDiagonal(TestCase):
+
+    def test_no_win(self):
+        mat = np.array([
+            [1, 1, 1, 0, 1],
+            [1, 2, 2, 1, 1],
+            [1, 1, 1, 2, 2],
+            [2, 2, 2, 1, 1]
+        ])
+        self.assertEqual(WinState.DRAW, game.check_win_in_diagonal(mat, 4))
+        self.assertEqual(WinState.DRAW, game.check_win_in_diagonal(mat[::-1], 4))
+
+        mat = np.array([
+            [1, 1, 1, 1, 1, 1],
+            [2, 2, 2, 2, 2, 2],
+            [1, 1, 1, 1, 1, 1],
+            [2, 2, 2, 2, 2, 2]
+        ])
+        self.assertEqual(WinState.DRAW, game.check_win_in_diagonal(mat, 4))
+        self.assertEqual(WinState.DRAW, game.check_win_in_diagonal(mat[::-1], 4))
+
+        mat = np.array([
+            [1, 1, 1, 1, 1, 1],
+            [2, 2, 1, 2, 2, 2],
+            [1, 1, 1, 2, 2, 1],
+            [2, 2, 2, 2, 2, 2]
+        ])
+        self.assertEqual(WinState.DRAW, game.check_win_in_diagonal(mat, 4))
+        self.assertEqual(WinState.DRAW, game.check_win_in_diagonal(mat[::-1], 4))
+
+        mat = np.array([
+            [1, 1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1, 1],
+            [0, 0, 0, 0, 0, 0],
+            [1, 1, 1, 1, 1, 1]
+        ])
+        self.assertEqual(WinState.DRAW, game.check_win_in_diagonal(mat, 4))
+        self.assertEqual(WinState.DRAW, game.check_win_in_diagonal(mat[::-1], 4))
+
+        mat = np.array([
+            [1, 1, 1, 0, 1, 1, 1],
+            [1, 1, 0, 1, 0, 1, 1],
+            [1, 0, 0, 1, 0, 0, 1],
+            [1, 0, 0, 1, 0, 0, 1],
+            [1, 1, 0, 1, 0, 1, 1],
+            [1, 1, 1, 0, 1, 1, 1],
+        ])
+        self.assertEqual(WinState.DRAW, game.check_win_in_diagonal(mat, 4))
+        self.assertEqual(WinState.DRAW, game.check_win_in_diagonal(mat[::-1], 4))
+
+    def test_win_in_main_diagonal(self):
+        mat = np.array([
+            [0, 0, 0, 1, 0, 0, 0],
+            [0, 0, 0, 0, 1, 0, 0],
+            [0, 0, 0, 0, 0, 1, 0],
+            [0, 0, 0, 0, 0, 0, 1],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+        ])
+        self.assertEqual(WinState.GREEN, game.check_win_in_diagonal(mat, 4))
+        self.assertEqual(WinState.DRAW, game.check_win_in_diagonal(mat[::-1], 4))
+
+        mat = np.array([
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 1, 0, 0, 0],
+            [0, 0, 0, 0, 1, 0, 0],
+            [0, 0, 0, 0, 0, 1, 0],
+            [0, 0, 0, 0, 0, 0, 1],
+            [0, 0, 0, 0, 0, 0, 0],
+        ])
+        self.assertEqual(WinState.GREEN, game.check_win_in_diagonal(mat, 4))
+        self.assertEqual(WinState.DRAW, game.check_win_in_diagonal(mat[::-1], 4))
+
+        mat = np.array([
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 1, 0, 0, 0, 0],
+            [0, 0, 0, 1, 0, 0, 0],
+            [0, 0, 0, 0, 1, 0, 0],
+            [0, 0, 0, 0, 0, 1, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+        ])
+        self.assertEqual(WinState.GREEN, game.check_win_in_diagonal(mat, 4))
+        self.assertEqual(WinState.DRAW, game.check_win_in_diagonal(mat[::-1], 4))
+
+        mat = np.array([
+            [1, 0, 0, 0, 0, 0, 0],
+            [0, 1, 0, 0, 0, 0, 0],
+            [0, 0, 1, 0, 0, 0, 0],
+            [0, 0, 0, 1, 0, 0, 0],
+            [0, 0, 0, 0, 1, 0, 0],
+            [0, 0, 0, 0, 0, 1, 0],
+        ])
+        self.assertEqual(WinState.GREEN, game.check_win_in_diagonal(mat, 4))
+        self.assertEqual(WinState.DRAW, game.check_win_in_diagonal(mat[::-1], 4))
+
+        mat = np.array([
+            [0, 0, 0, 0, 0, 0, 0],
+            [1, 0, 0, 0, 0, 0, 0],
+            [0, 1, 0, 0, 0, 0, 0],
+            [0, 0, 1, 0, 0, 0, 0],
+            [0, 0, 0, 1, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+        ])
+        self.assertEqual(WinState.GREEN, game.check_win_in_diagonal(mat, 4))
+        self.assertEqual(WinState.DRAW, game.check_win_in_diagonal(mat[::-1], 4))
+
+        mat = np.array([
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [1, 0, 0, 0, 0, 0, 0],
+            [0, 1, 0, 0, 0, 0, 0],
+            [0, 0, 1, 0, 0, 0, 0],
+            [0, 0, 0, 1, 0, 0, 0],
+        ])
+        self.assertEqual(WinState.GREEN, game.check_win_in_diagonal(mat, 4))
+        self.assertEqual(WinState.DRAW, game.check_win_in_diagonal(mat[::-1], 4))
+
+    def test_win_in_sub_diagonal(self):
+        mat = np.array([
+            [0, 0, 0, 1, 0, 0, 0],
+            [0, 0, 1, 0, 0, 0, 0],
+            [0, 1, 0, 0, 0, 0, 0],
+            [1, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+        ])
+        self.assertEqual(WinState.DRAW, game.check_win_in_diagonal(mat, 4))
+        self.assertEqual(WinState.GREEN, game.check_win_in_diagonal(mat[::-1], 4))
+
+        mat = np.array([
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 1, 0, 0, 0],
+            [0, 0, 1, 0, 0, 0, 0],
+            [0, 1, 0, 0, 0, 0, 0],
+            [1, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+        ])
+        self.assertEqual(WinState.DRAW, game.check_win_in_diagonal(mat, 4))
+        self.assertEqual(WinState.GREEN, game.check_win_in_diagonal(mat[::-1], 4))
+
+        mat = np.array([
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 1, 0, 0],
+            [0, 0, 0, 1, 0, 0, 0],
+            [0, 0, 1, 0, 0, 0, 0],
+            [0, 1, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+        ])
+        self.assertEqual(WinState.DRAW, game.check_win_in_diagonal(mat, 4))
+        self.assertEqual(WinState.GREEN, game.check_win_in_diagonal(mat[::-1], 4))
+
+        mat = np.array([
+            [0, 0, 0, 0, 0, 0, 1],
+            [0, 0, 0, 0, 0, 1, 0],
+            [0, 0, 0, 0, 1, 0, 0],
+            [0, 0, 0, 1, 0, 0, 0],
+            [0, 0, 1, 0, 0, 0, 0],
+            [0, 1, 0, 0, 0, 0, 0],
+        ])
+        self.assertEqual(WinState.DRAW, game.check_win_in_diagonal(mat, 4))
+        self.assertEqual(WinState.GREEN, game.check_win_in_diagonal(mat[::-1], 4))
+
+        mat = np.array([
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 1],
+            [0, 0, 0, 0, 0, 1, 0],
+            [0, 0, 0, 0, 1, 0, 0],
+            [0, 0, 0, 1, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+        ])
+        self.assertEqual(WinState.DRAW, game.check_win_in_diagonal(mat, 4))
+        self.assertEqual(WinState.GREEN, game.check_win_in_diagonal(mat[::-1], 4))
+
+        mat = np.array([
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 1],
+            [0, 0, 0, 0, 0, 1, 0],
+            [0, 0, 0, 0, 1, 0, 0],
+            [0, 0, 0, 1, 0, 0, 0],
+        ])
+        self.assertEqual(WinState.DRAW, game.check_win_in_diagonal(mat, 4))
+        self.assertEqual(WinState.GREEN, game.check_win_in_diagonal(mat[::-1], 4))
+
+
+class TestGameGetWinState(TestCase):
+
+    def test_win_in_row(self):
+        g = grid.Grid(6, 7)
+
+        g[5, 3] = Chip.GREEN
+        g[5, 4] = Chip.RED
+        g[5, 5] = Chip.GREEN
+        g[5, 6] = Chip.GREEN
+
+        g[4, 3] = Chip.RED
+        g[4, 4] = Chip.RED
+        g[4, 5] = Chip.RED
+        g[4, 6] = Chip.RED
+
+        ga = game.Game(grid=g, chips_in_a_row=4)
+        self.assertEqual((WinState.RED, 'row'), ga.get_win_state())
+
+    def test_win_in_col(self):
+        g = grid.Grid(6, 7)
+
+        g.drop_chip(1, Chip.GREEN)
+        g.drop_chip(1, Chip.RED)
+        g.drop_chip(1, Chip.RED)
+        g.drop_chip(1, Chip.RED)
+        g.drop_chip(1, Chip.RED)
+
+        ga = game.Game(grid=g, chips_in_a_row=4)
+        self.assertEqual((WinState.RED, 'col'), ga.get_win_state())
+
+    def test_win_in_main_diagonal(self):
+        g = grid.Grid(6, 7)
+
+        g[5, 0] = Chip.RED
+        g[4, 0] = Chip.RED
+        g[3, 0] = Chip.RED
+        g[2, 0] = Chip.GREEN
+
+        g[5, 1] = Chip.RED
+        g[4, 1] = Chip.RED
+        g[3, 1] = Chip.GREEN
+
+        g[5, 2] = Chip.RED
+        g[4, 2] = Chip.GREEN
+
+        g[5, 3] = Chip.GREEN
+
+        ga = game.Game(grid=g, chips_in_a_row=4)
+        self.assertEqual((WinState.GREEN, 'main diag'), ga.get_win_state())
+
+    def test_win_in_sub_diagonal(self):
+        g = grid.Grid(6, 7)
+
+        g[5, 0] = Chip.RED
+
+        g[5, 1] = Chip.GREEN
+        g[4, 1] = Chip.RED
+
+        g[5, 2] = Chip.GREEN
+        g[4, 2] = Chip.GREEN
+        g[3, 2] = Chip.RED
+
+        g[5, 3] = Chip.GREEN
+        g[4, 3] = Chip.GREEN
+        g[3, 3] = Chip.GREEN
+        g[2, 3] = Chip.RED
+
+        ga = game.Game(grid=g, chips_in_a_row=4)
+        self.assertEqual((WinState.RED, 'sub diag'), ga.get_win_state())
